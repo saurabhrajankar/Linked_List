a19e90972bf93a7da7d1236442e9ccf4142be8ff
saurabhrajankar <112363658+saurabhrajankar@users.noreply.github.com>
2022-10-19 12:01:27 +0530

    create Linked List by adding 30 and 56 to 70

---
+++ UC2-Adding_30_and_56_to_70/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC2_Adding_30_and_56_to_70
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+    public class LinkedList
+    {
+        Node head = null;
+        public void add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+                Console.WriteLine(node.data + ":Inserted SuccessFully");
+            }
+            else
+            {
+                Node addr = new Node(data);
+                Node h1 = null;
+                h1 = this.head;
+                this.head = addr;
+                Node temp = addr;
+                temp.next = h1;
+                Console.WriteLine(addr.data + ":Inserted SuccessFully");
+            }
+        }
+        public void Display()
+        {
+            if (this.head == null)
+            {
+                Console.WriteLine("Empty");
+            }
+            else
+            {
+                Console.WriteLine("------------------------------------------------------");
+                Console.WriteLine("Values In LinkedList Are:");
+                while (this.head != null)
+                {
+                    Console.Write(this.head.data + " ");
+                    this.head = this.head.next;
+                }
+            }
+        }
+    }
+    internal class main
+    {
+        static void Main(string[] args)
+        {
+            LinkedList l = new LinkedList();
+            l.add(70);
+            //l.Display();
+            //Console.WriteLine("After Insertion Of 30");
+            l.add(30);
+            //l.Display();
+            //Console.WriteLine("After Insertion Of 56");
+            l.add(56);
+            l.Display();
+        }
+    }
+}
---
+++ UC2-Adding_30_and_56_to_70/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UC2-Adding_30_and_56_to_70")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("UC2-Adding_30_and_56_to_70")]
+[assembly: AssemblyCopyright("Copyright © HP 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("609b9f65-2427-4180-ad51-12d4f3c871a8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
