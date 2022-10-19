27b7c49810017648b3388f5ea623a12ee44d5513
saurabhrajankar <saurabhrajankar777@gmail.com>
2022-10-19 11:52:22 +0530

    Linked List

---
+++ Linked_List/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linked_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome The Linked List");
+            Console.ReadLine(); 
+        }
+    }
+}
