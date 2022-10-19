132db6d23ba9224b964ff48e599ef8180eca0f1f
saurabhrajankar <112363658+saurabhrajankar@users.noreply.github.com>
2022-10-19 12:03:52 +0530

    delete the first element in the LinkedList of seq.

---
+++ UC5-Delete_First_Element/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC5_Delete_First_Element
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+    public class LinkedList
+    {
+        Node head = null;
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine(node.data + ":Inserted SuccessFully");
+        }
+        internal void popfirst()
+        {
+            this.head = this.head.next;
+            Console.WriteLine("First Element Removed");
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList Is Empty");
+            }
+            else
+            {
+                Console.WriteLine("Values In LinkedList Are:");
+                while (temp != null)
+                {
+                    Console.WriteLine(temp.data + " ");
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+    internal class LL5
+    {
+        static void Main(string[] args)
+        {
+            LinkedList l = new LinkedList();
+            l.Add(56);
+            l.Add(30);
+            l.Add(70);
+            Console.WriteLine("----------------------------------");
+            l.Display();
+            Console.WriteLine("----------------------------------");
+            l.popfirst();
+            l.Display();
+        }
+    }
+}
